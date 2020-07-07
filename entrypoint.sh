1c057a2e50678fafa8356e5793ef929fe37deabc
imairi <imairi@gmail.com>
2020-07-07 17:30:25 +0900

    fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,23 +3,26 @@
 # convert swiftlint's output into GitHub Actions Logging commands
 # https://help.github.com/en/github/automating-your-workflow-with-github-actions/development-tools-for-github-actions#logging-commands
 
-function stripPWD() {
-    sed -E "s/$(pwd|sed 's/\//\\\//g')\///"
-}
+#function stripPWD() {
+#    sed -E "s/$(pwd|sed 's/\//\\\//g')\///"
+#}
 
-function convertToGitHubActionsLoggingCommands() {
-    sed -E 's/^(.*):([0-9]+):([0-9]+): (warning|error|[^:]+): (.*)/::\4 file=\1,line=\2,col=\3::\5/'
-}
+#function convertToGitHubActionsLoggingCommands() {
+#    sed -E 's/^(.*):([0-9]+):([0-9]+): (warning|error|[^:]+): (.*)/::\4 file=\1,line=\2,col=\3::\5/'
+#}
 
-if ! ${DIFF_BASE+false};
-then
-	changedFiles=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD $DIFF_BASE) -- '*.swift')
+#if ! ${DIFF_BASE+false};
+#then
+#	changedFiles=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD $DIFF_BASE) -- '*.swift')
+#
+#	if [ -z "$changedFiles" ]
+#	then
+#		echo "No Swift file changed"
+#		exit
+#	fi
+#fi
 
-	if [ -z "$changedFiles" ]
-	then
-		echo "No Swift file changed"
-		exit
-	fi
-fi
-
-set -o pipefail && swiftlint "$@" -- $changedFiles | stripPWD | convertToGitHubActionsLoggingCommands
+#set -o pipefail && swiftlint "$@" -- $changedFiles | stripPWD | convertToGitHubActionsLoggingCommands
+pwd
+ls -l
+swiftlint autocorrect
